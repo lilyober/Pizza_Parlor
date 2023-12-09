d3e2f0bf68093888630a5b0940081495ea0403e4
lilyob <lilyober07@gmail.com>
2023-12-09 10:19:41 -0800

    Create working pizzaType function.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-Descibe:  
+Descibe: pizzaType()
+
+Test: "It should creates the type of pizza based on toppings and size."
+Code: let pepPizza = new pizzaType("Pepparoni", "Small")
+Expected output: pepPizza {topping: "Pepparoni", "Small"}
+
+Descibe: pizzaType.prototype.price = function (num) {}
+
+Test:
+Code:
+Expected output: 
--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,11 @@
 // Business Logic 
-    // object constructor for toppings and size
-    // prototype method for selections -- use own formula 
+// object constructor for toppings and size
+// prototype method for selections -- use own formula 
 
-
+function pizzaType(topping, size) {
+    this.topping = topping;
+    this.size = size;
+}
 
 
 
