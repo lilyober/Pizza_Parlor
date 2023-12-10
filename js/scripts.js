27bfa3856d25bea48414c5cfc0dffd5bab2c442c
lilyob <lilyober07@gmail.com>
2023-12-10 09:58:28 -0800

    WIP UI logic.

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,18 @@
 // object constructor for toppings and size
 // prototype method for selections and cost -- use own formula 
 
-function pizzaType(topping, size) {
+function pizzaType(topping) {
     this.topping = topping;
-    this.size = size;
+    // this.size = size;
 }
 
 pizzaType.prototype.priceIs = function (topping) {
     if (topping === "Cheese") {
-        this.topping = "$8";
+        this.topping.push(8);
     } else if (topping === "Pepperoni" || topping === "Veggie") {
-        this.topping = "$10";
+        this.topping.push(10);
     }
-    return this.topping;
-
+    return this.topping.toString();
 };
 
 // UI logic 
@@ -23,12 +22,13 @@ window.onload = function () {
     let form = document.getElementById("form");
     form.addEventListener("submit", (event) => {
         event.preventDefault();
+        let newPizza = new pizzaType(" ")
         let toppingsSelection = document.getElementById("toppings").value;
 
         // let price = pizzaType;
-        // toppingsSelection.priceIs;
+        newPizza.priceIs(toppingsSelection);
 
-        document.querySelector("span#results").innerText = toppingsSelection.priceIs;
+        document.querySelector("span#results").innerText = pizzaType();
         document.querySelector("div#resetButton").removeAttribute("class");
     });
 }
