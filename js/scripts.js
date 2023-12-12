738902c67ff21501d602c2e01bd1c4673bae0726
lilyob <lilyober07@gmail.com>
2023-12-11 18:41:55 -0800

    Shows price of pizza.

--- js/scripts.js
+++ js/scripts.js
@@ -7,9 +7,9 @@ function pizzaType(topping, size) {
 
 pizzaType.prototype.priceIs = function (topping) {
     if (this.topping === "Cheese") {
-        this.price.push("8");
+        this.price.push("$8");
     } else if (this.topping === "Pepperoni" || this.topping === "Veggie") {
-        this.price.push("10");
+        this.price.push("$10");
     }
     return this.price;
 };
@@ -18,12 +18,9 @@ window.onload = function () {
     let form = document.getElementById("form");
     form.addEventListener("submit", (event) => {
         event.preventDefault();
-        let toppingsSelection = document.getElementById("toppings").innerText;
-        // let newPizza = new pizzaType(toppingSelection);
 
-        // newPizza.priceIs(toppingsSelection);
-        console.log(pizzaType.priceIs);
-        document.querySelector("span#results").innerText = this.price;
+        let newPizza = new pizzaType(document.getElementById("toppings").value);
+        document.querySelector("span#results").innerText = newPizza.priceIs();
         document.querySelector("div#resetButton").removeAttribute("class");
     });
 }
